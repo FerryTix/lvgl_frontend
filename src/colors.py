7a2e84251ebd848e51fa8b014d71c9298ce9a316
elsholz <hendrik.lankers.hl@googlemail.com>
2021-04-05 12:25:39 +0200

    create color constants

---
+++ src/colors.py
@@ -0,0 +1,10 @@
+from enum import Enum
+import lvgl as lv
+
+class Colors(Enum):
+    BACKGROUND = lv.color_hex(0xffffff)
+    GRAY = lv.color_hex(0xf3f3f3)
+    GRAY2 = lv.color_hex(0xe5e5e5)
+    GREEN = lv.color_hex(0x00954d)
+    BLUE =  lv.color_hex(0x4c567c)
+    TEAL = lv.color_hex(0x007062)
